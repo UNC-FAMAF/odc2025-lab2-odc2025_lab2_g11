1d537e92533c50d690270c37ac30cc5c5f39a55f
BGfamaf <bruna.grosso@mi.unc.edu.ar>
2025-05-26 16:58:36 -0300

    pruebas parametrizadas

--- ejercicio1/pruebas.s
+++ ejercicio1/pruebas.s
@@ -1,25 +1,29 @@
-//rectangulo en el medio
-//paso 1 guardar constantes de ancho y alto van a ser contadores
+calculardireccion:
+// paramentros:
+//x3 -> x
+//x4-> y
+//queda en x5 la direcion de (x,y)
 
-.equ ANCHOREC, 300
-.equ ALTOREC,  200
-
-// paso 2 guardar color y constante altorec en registro
-movz x11, 0xFF, lsl 16
-movk x11, 0x0000, lsl 0
-
-mov x2, ALTOREC             // contador de filas
-
-// //paso 3 encontrar direccion base para arrancar en rectangulo del pixel que querramos, en este caso elijo el pixel (200,120) x=200 y=120
-//usando la formula direcion=base + 4*(x + (y*640))  
-mov x3, #200                // x
-mov x4, #120                // y
 mov x6, SCREEN_WIDTH        // ancho pantalla = 640
-// elijo x5 como registro base
-mul x5, x6, x4              // y * 640
-add x5, x5, x3              // x + y*640
-lsl x5, x5, #2              // 4*(x + (y*640)
-add x5, x20, x5              // base + 4*(x + (y*640))
+
+mul x5, x6, x4              // y * ancho
+add x5, x5, x3              // x + y*ancho
+lsl x5, x5, #2              // *4 → offset en bytes
+add x5, x20, x5              // framebuffer base + offset
+
+ret
+
+rectangulo:
+//parametros 
+// x1 -> ancho del rectangulo
+// x2 -> alto del rectangulo
+// x3 -> x
+// x4 -> y
+// x11-> color
+ 
+ mov x6, x2
+ 
+ bl calculardireccion
 
 // Precalcular tamaño fila
 mov x8, #640
@@ -27,14 +31,15 @@ lsl x8, x8, #2              // x8 = 640 * 4 = 2560
 
 // Dibujo del rectángulo
 loop2:                      // filas
-    mov x1, ANCHOREC        // columnas restantes
+    mov x9, x1        // columnas restantes ancho rectangulo #200
     mov x7, x5              // puntero a comienzo de fila
 loop3:
     stur w11, [x7]          // pintar píxel
     add x7, x7, #4          // avanzar a próximo píxel
-    sub x1, x1, #1
-    cbnz x1, loop3          // volver si no terminó la fila
+    sub x9, x9, #1
+    cbnz x9, loop3          // salta a loop3 si no terminó de pintar cada pixel de la fila
 
     add x5, x5, x8          // avanzar a próxima fila
-    sub x2, x2, #1
-    cbnz x2, loop2
+    sub x6, x6, #1
+    cbnz x6, loop2
+ret   
