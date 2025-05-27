5599a299a23123e448e67c738ca9f56cd6547807
BGfamaf <bruna.grosso@mi.unc.edu.ar>
2025-05-27 15:32:01 -0300

    Update pruebas.s

--- ejercicio1/pruebas.s
+++ ejercicio1/pruebas.s
@@ -2,14 +2,14 @@ calculardireccion:
 // paramentros:
 //x3 -> x
 //x4-> y
-//queda en x5 la direcion de (x,y)
+//queda en x0 la direcion de (x,y)
 
 mov x6, SCREEN_WIDTH        // ancho pantalla = 640
 
-mul x5, x6, x4              // y * ancho
-add x5, x5, x3              // x + y*ancho
-lsl x5, x5, #2              // *4 → offset en bytes
-add x5, x20, x5              // framebuffer base + offset
+mul x0, x6, x4              // y * ancho
+add x0, x0, x3              // x + y*ancho
+lsl x0, x0, #2              // *4 → offset en bytes
+add x0, x20, x0              // framebuffer base + offset
 
 ret
 
@@ -21,7 +21,7 @@ rectangulo:
 // x4 -> y
 // x11-> color
  
- mov x6, x2
+ mov x5, x2
  
  bl calculardireccion
 
@@ -32,14 +32,14 @@ lsl x8, x8, #2              // x8 = 640 * 4 = 2560
 // Dibujo del rectángulo
 loop2:                      // filas
     mov x9, x1        // columnas restantes ancho rectangulo #200
-    mov x7, x5              // puntero a comienzo de fila
+    mov x7, x0              // puntero a comienzo de fila
 loop3:
     stur w11, [x7]          // pintar píxel
     add x7, x7, #4          // avanzar a próximo píxel
     sub x9, x9, #1
     cbnz x9, loop3          // salta a loop3 si no terminó de pintar cada pixel de la fila
 
-    add x5, x5, x8          // avanzar a próxima fila
-    sub x6, x6, #1
-    cbnz x6, loop2
+    add x0, x0, x8          // avanzar a próxima fila
+    sub x5, x5, #1
+    cbnz x5, loop2
 ret   
